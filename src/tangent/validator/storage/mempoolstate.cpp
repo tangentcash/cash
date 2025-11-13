9a5c0c21467859e3232271faba4d31f13917b9a9
Roman Punia <me@romanpunia.com>
2025-11-13 13:46:20 +0300

    New discovery comparator

--- src/tangent/validator/storage/mempoolstate.cpp
+++ src/tangent/validator/storage/mempoolstate.cpp
@@ -337,15 +337,12 @@ namespace tangent
 		expects_lr<vector<node_location_pair>> mempoolstate::get_neighbor_nodes_with(size_t offset, size_t count, uint32_t services)
 		{
 			schema_list map;
-			if (services > 0)
-			{
-				map.push_back(var::set::integer(services));
-				map.push_back(var::set::integer(services));
-			}
+			map.push_back(var::set::integer(services));
+			map.push_back(var::set::integer(services));
 			map.push_back(var::set::integer(count));
 			map.push_back(var::set::integer(offset));
 
-			auto cursor = get_storage().emplace_query(__func__, stringify::text("SELECT account, address FROM nodes WHERE quality IS NOT NULL %s ORDER BY quality DESC LIMIT ? OFFSET ?", services > 0 ? "AND services & ? == ?" : ""), &map);
+			auto cursor = get_storage().emplace_query(__func__, "SELECT account, address FROM nodes WHERE quality IS NOT NULL AND (services & ?) == ? ORDER BY quality DESC LIMIT ? OFFSET ?", &map);
 			if (!cursor || cursor->error())
 				return expects_lr<vector<node_location_pair>>(layer_exception(ledger::storage_util::error_of(cursor)));
 
