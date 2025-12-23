94073be0eedeca981af1ae8cec9fa8628c185e88
Roman Punia <me@romanpunia.com>
2025-12-23 07:19:47 +0300

    Fix: tx log

--- src/tangent/service/superchain.cpp
+++ src/tangent/service/superchain.cpp
@@ -528,7 +528,7 @@ namespace tangent
 
 			auto result = coawait(implementation->prepare_transaction(*normalized_from_link, normalized_to, normalized_max_fee));
 			if (protocol::now().user.superchain.logging)
-				VI_INFO("%s built transaction: %s", blockchain.c_str(), result ? result->as_tree().as_json() : result.error().what());
+				VI_INFO("%s built transaction: %s", blockchain.c_str(), result ? result->as_tree().as_json().c_str() : result.error().what());
 			coreturn result;
 		}
 		expects_lr<finalized_transaction> server_node::finalize_transaction(const algorithm::asset_id& asset, prepared_transaction&& prepared)
