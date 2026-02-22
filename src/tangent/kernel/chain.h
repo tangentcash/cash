f788031bda1ffa05b9663fd534438df9b1e7d4a1
etherealcardinal <etherc@etherc.eu>
2026-02-22 14:59:08 +0300

    Consensus: enforce minimal participation size of 7 per each bridge account

--- src/tangent/kernel/chain.h
+++ src/tangent/kernel/chain.h
@@ -293,7 +293,7 @@ namespace tangent
 			struct
 			{
 				uint64_t locking_time = 691200000;
-				uint64_t min_per_account = 5;
+				uint64_t min_per_account = 7;
 				uint64_t max_per_account = 23;
 				decimal min_stake_value = std::string_view("2700");
 				decimal stake_threshold = std::string_view("0.20");
