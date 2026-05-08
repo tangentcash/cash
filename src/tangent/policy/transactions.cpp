e118e575903d804ea82f7217b196a21974273a5c
etherc <etherealcardinal@proton.me>
2026-05-08 23:03:00 +0300

    Fix: validation procedure naming

--- src/tangent/policy/transactions.cpp
+++ src/tangent/policy/transactions.cpp
@@ -2928,7 +2928,7 @@ namespace tangent
 					return layer_exception("witness transaction fee overflow (max: " + bridge->fee_rate.to_string() + ")");
 			}
 
-			auto rounding_equals_check = [](const decimal& a, const decimal& b)
+			auto check_truncated = [](const decimal& a, const decimal& b)
 			{
 				if (a == b)
 					return true;
@@ -2945,14 +2945,14 @@ namespace tangent
 				{
 					if (it->second.is_nan())
 						continue;
-					else if (output_asset != base_asset && !rounding_equals_check(actual_output_value, it->second))
+					else if (output_asset != base_asset && !check_truncated(actual_output_value, it->second))
 						return layer_exception("witness transaction output pays unexpected token value");
 					else if (output_asset == base_asset && ((actual_output_value < it->second - bridge->fee_rate) || (actual_output_value > it->second + bridge->fee_rate)))
 						return layer_exception("witness transaction output pays unexpected native value");
 				}
 				else if (output_asset == base_asset && actual_output_value > std::max(max_change_value, bridge->fee_rate))
 					return layer_exception("witness transaction output pays unexpected native value");
-				else if (output_asset != base_asset && rounding_equals_check(actual_output_value, max_change_value))
+				else if (output_asset != base_asset && !check_truncated(actual_output_value, max_change_value))
 					return layer_exception("witness transaction output pays unexpected token value");
 			}
 
